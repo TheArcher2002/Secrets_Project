eb900b558d3116ec3c0d6e58e370f36be6216975
TheArcher2002 <harikrishnanmnair2002@gmail.com>
2023-08-23 19:50:47 +0530

    Thirtiary Commit

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 // const encrypt = require("mongoose-encryption");
-const md5 = require('md5');
+// const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const app = express();
 //console.log(process.env.API_KEY);
 
@@ -30,13 +32,15 @@ app.route("/login")
 })
 .post((req,res)=>{
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
  
     User.findOne({email : username })
     .then((foundUser)=> {
-        if(foundUser.password === password){
-            res.render("secrets");
-        }
+        bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
+            if(result == true){
+                res.render("secrets");
+            }
+        });
     })
     .catch((err)=> console.log(err));
 })
@@ -46,13 +50,15 @@ app.route("/register")
     res.render("register");
 })
 .post((req,res)=>{
-    const newUser = new User({
-        email : req.body.username,
-        password : md5(req.body.password)
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email : req.body.username,
+            password : hash
+        });
+        newUser.save()
+        .then(()=> res.render("secrets"))
+        .catch((err)=> console.log(err))
     });
-    newUser.save()
-    .then(()=> res.render("secrets"))
-    .catch((err)=> console.log(err))
 });
 
 app.listen(3000, function() {
